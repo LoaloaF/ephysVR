uV overflow fix, imp analysis

mea1k_connectivity_scripts/analyze_invivo_imp.py
@@ -8,16 +8,18 @@ from CustomLogger import CustomLogger as Logger
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 import ephys_constants as C
 from mea1k_modules.mea1k_raw_preproc import read_raw_data
 from mea1k_modules.mea1k_raw_preproc import read_stim_DAC
+from mea1k_modules.mea1k_raw_preproc import get_raw_implant_mapping
 from mea1k_modules.mea1k_raw_preproc import get_recording_implant_mapping
-from mea1k_modules.mea1k_visualizations import vis_shank_traces, viz_mea1k_config
-# from signal_helpers import estimate_frequency_power
+from signal_helpers import estimate_frequency_power
 
-
-from mea1k_viz import draw_mea1k
+from mea1k_modules.mea1k_visualizations import vis_shank_traces, viz_mea1k_config, draw_interconnect_pads
+from mea1k_modules.mea1k_visualizations import draw_mea1k
+from mea1k_modules.mea1k_visualizations import draw_mea1K_colorbar
 
 def get_hdf5_fnames_from_dir(subdir):
     fnames, ids = [], []
@@ -38,69 +40,143 @@ def get_hdf5_fnames_from_dir(subdir):
 def save_output(subdir, data, fname):
     fullpath = os.path.join(subdir, "processed")
     if not os.path.exists(fullpath):
-        print("creating processed output dir")
+        print("creating processed output dir: ", fullpath)
         os.makedirs(fullpath)
+    print("Saving to: ", os.path.join(fullpath, fname))
     data.to_csv(os.path.join(fullpath, fname))
 
-def extract_impedance(subdir, implant_name, debug=False):
+def extract_impedance(subdir, implant_name, current_ampl_nA, debug=False):
+    L = Logger()
     fnames, ids = get_hdf5_fnames_from_dir(subdir)
-    all_data = []
+    aggr_imp_data = []
     for fname, i in zip(fnames, ids):
-        print(f"Config {i} of {len(fnames)}")
+        L.logger.info(f"Config {i},{fname} of {len(fnames)}")
         
+        # if int(i[-4:]) != 1200:
+        #     continue
+        # get the config information about this configuration
         stimulated = pd.read_csv(os.path.join(subdir, fname.replace(".raw.h5", ".csv")))
-        stim_mea1k_el = stimulated[stimulated.stim].electrode.values
-        print(stim_mea1k_el)
 
-        # if fname == "el_config_S1D1650.raw.h5":
-        if True:
-            dac = read_stim_DAC(subdir, fname)
-            data = read_raw_data(subdir, fname, convert2uV=True,
-                                subtract_dc_offset=False, col_slice=slice(None, None, None))
 
-            mapping = get_recording_implant_mapping(subdir, fname, implant_name=implant_name)
-            viz_mea1k_config(mapping, stim_mea1k_el=stim_mea1k_el)
-            
-            vis_shank_traces(data, mapping, stim_mea1k_el=stim_mea1k_el, scaler=4/1000)
+        # dac = read_stim_DAC(subdir, fname)
+        # stim_sample_ids = np.where(dac != 512)[0]
+        # shortcut, since we know the stim samples are between 20500 and 29500
+        stim_sample_ids = (20500, 29500)
+        data = read_raw_data(subdir, fname, convert2uV=True,
+                            subtract_dc_offset=False, 
+                            col_slice=slice(stim_sample_ids[0], stim_sample_ids[-1], None))
+        
+        if debug:
+            mapping = get_recording_implant_mapping(subdir, fname, implant_name=implant_name,
+                                                    drop_non_bonded=False)
+            viz_mea1k_config(mapping, stim_mea1k_el=stimulated.electrode[stimulated.stim].item())
+            # vis_shank_traces(data, mapping, stim_mea1k_el=stimulated.electrode[stimulated.stim].item(), scaler=1/1_000, uVrange=470_000)
             
             plt.subplot(2, 1, 1)
             plt.plot(data.T)
-            plt.subplot(2, 1, 2, sharex=plt.gca())  
-            plt.plot(dac)
+            plt.subplot(2, 1, 2, sharex=plt.gca())              
+            plt.plot(read_stim_DAC(subdir, fname))
             plt.show()
+    
+        mean_ampl = []
+        for j,row in enumerate(data):
+            if stimulated.stim[j]:
+                pass
+            _, m_ampl = estimate_frequency_power(row.astype(float), 
+                                                    sampling_rate=C.SAMPLING_RATE, 
+                                                    debug=debug, 
+                                                    min_band=960, max_band=1040)
+            mean_ampl.append(m_ampl)
+        mean_ampl = np.array(mean_ampl)
         
-        continue
-        
-        
+        stimulated['imp_voltage_uV'] = mean_ampl
+        stimulated['imp_kOhm'] = (mean_ampl / (current_ampl_nA * 1e-3)) / 1e3 * stimulated.stim.astype(int)
+        stimulated['imp_stim_ratio'] = mean_ampl/ mean_ampl[stimulated.stim].item()
+        stimulated.drop(columns=['channel', 'x', 'y', 'stim'], inplace=True)
+        stimulated.index = pd.MultiIndex.from_product([[fname.replace(".raw.h5","")],
+                                                        stimulated.index], names=['config', 'el'])
+        L.logger.debug(stimulated)
+        aggr_imp_data.append(stimulated)
+    
+    aggr_imp_data = pd.concat(aggr_imp_data)
+    save_output(subdir, aggr_imp_data, "extracted_imp_voltages.csv")
 
-        # print("Filtering...")
-        # mean_ampl = []
-        # for j,row in enumerate(data.values):
-        #     debug = True if j <10 and debug else False
-        #     _, m_ampl = estimate_frequency_power(row, sampling_rate=C.SAMPLING_RATE, 
-        #                                          debug=debug, min_band=960, max_band=1040)
-        #     mean_ampl.append(m_ampl)
         
-        # data = pd.DataFrame(mean_ampl, index=data.index, columns=['ampl'])
-        # data['connectivity'] = data.ampl.values/input_ampl_mV 
-        # data['input_ampl_mV'] = input_ampl_mV
-        # data.index = pd.MultiIndex.from_product([[i],data.index], names=['config', 'el'])
-        # print(f"Done. n >80%: {(data.connectivity >.8).sum()}\n")
+    
+def vis_impedance(subdir, implant_name):
+    data = pd.read_csv(os.path.join(subdir, "processed", "extracted_imp_voltages.csv"))
+    data = data[data.stim_unit.notna()]
+    data.sort_values(by='pad_id', inplace=True)
+    
+    # draw thw connectivty of the chip
+    mapping = get_raw_implant_mapping(implant_name=implant_name)
+    colors = np.stack([mapping.sort_values('mea1k_el').mea1k_connectivity.values]*3, axis=1)
+    colors = np.clip(colors, 0, .75)
+    (fig, ax), el_rects = draw_mea1k(el_color=list(colors))
+    
+    imp = np.clip(data.imp_kOhm.values, 0, 6.5*1e2)
+    # Use the hsv colormap
+    cmap = plt.get_cmap('plasma')
+    norm = plt.Normalize(vmin=0, vmax=6.5*1e2)
+    imp_colors = cmap(norm(imp))
+    imp_colors = {pad_id: imp_colors[i][:3] for i, pad_id in enumerate(data.pad_id)}
 
+    draw_mea1K_colorbar(cmap, norm, 'Impedance [kOhm]', orientation='vertical')
+    draw_interconnect_pads(mapping, edgecolor=imp_colors, draw_on_ax=ax, pad_alpha=.6)
+    plt.savefig(os.path.join(subdir, "processed", "impedance.png"))
+
+    cmap = plt.get_cmap('hsv')
+    norm = plt.Normalize(vmin=0, vmax=32)
+    stimunit_color = {pad_id: cmap(norm(stim_unit)) 
+                      for pad_id, stim_unit in zip(data.pad_id, data.stim_unit)}
+    
+    plt.figure()
+    plt.scatter(data.pad_id, imp, color=list(stimunit_color.values()))
+    # plt.ylim(0, 2500)
+    # vertical line at 0, 125, 1628/2+125, 1628/2+125+125
+    plt.axvline(0, color='k', linestyle='dashed')
+    plt.axvline(106, color='k', linestyle='dashed')
+    plt.axvline(814, color='k', linestyle='dashed')
+    plt.axvline(920, color='k', linestyle='dashed')
+    #save
+    
+def compare_impedance(subdirs, implant_name):
+    for subdir in subdirs:
+        data = pd.read_csv(os.path.join(subdir, "processed", "extracted_imp_voltages.csv"))
+        data = data[data.stim_unit.notna()]
+        data.sort_values(by='pad_id', inplace=True)
+        imp = np.clip(data.imp_kOhm.values, 0, 6.5*1e3)
+        [plt.axvline(x, color='k', linestyle='dashed') for x in [0, 106, 814, 920]]
+        plt.scatter(data.pad_id, imp, alpha=.6, label=subdir.split('/')[-1])
+    plt.legend()
+    plt.show()
+    
 def main(): 
     L = Logger()
-    L.init_logger(None, None, "DEBUG")
+    L.init_logger(None, None, "INFO")
     L.logger.debug("Starting in vivo impedance analysis")
     nas_dir = C.device_paths()[0]
     
-    implant_name = "241016_MEA1K03_H1278pad4shankB5"
+    # implant_name = "241016_MEA1K03_H1278pad4shankB5"
     # implant_name = "4983"
+    implant_name = "250308_MEA1K07_H1628pad1shankB6"
+    current_ampl_nA = 10 
+    
     subdirs = [
-        f"devices/implant_devices/{implant_name}/recordings/14:56:47_invivo_localstim_mode='small_current'_stimpulse='sine'_amplitude=3"
-        # f"devices/well_devices/{implant_name}/recordings/18:27:08_invivo_localstim_mode='small_current'_stimpulse='sine'2_amplitude=20"
+        # f"devices/well_devices/{implant_name}/recordings/18.27.08_invivo_localstim_mode='small_current'_stimpulse='sine'2_amplitude=20",
+        # f"devices/implant_devices/{implant_name}/recordings/17.07.54_GND_REF_cable_imp5_localstim_mode='small_current'_stimpulse='sine'2_amplitude=10",
+        f"devices/implant_devices/{implant_name}/recordings/00_17.07.54_GND_REF_cable_imp5_localstim_mode='small_current'_stimpulse='sine'2_amplitude=10",
+        f"devices/implant_devices/{implant_name}/recordings/01_10.35.26_GND_REF_cable_imp6_localstim_mode='small_current'_stimpulse='sine'2_amplitude=10",
+        f"devices/implant_devices/{implant_name}/recordings/02_14.06.56_noSilk_GND_REF_cable_imp7_localstim_mode='small_current'_stimpulse='sine'2_amplitude=10",
+        # f"devices/implant_devices/{implant_name}/recordings/03_16.01.21_noSilk_tapwater_GND_REF_cable_imp7_localstim_mode='small_current'_stimpulse='sine'2_amplitude=10",
     ]
     # el_config_S1D1650.raw.h5
-    extract_impedance(os.path.join(nas_dir, subdirs[0]), implant_name=implant_name)
+    # extract_impedance(os.path.join(nas_dir, subdirs[0]), implant_name=implant_name, 
+    #                   current_ampl_nA=current_ampl_nA, debug=False)
+    # vis_impedance(os.path.join(nas_dir, subdirs[0]), implant_name=implant_name)
+    compare_impedance([os.path.join(nas_dir, subdir) for subdir in subdirs], implant_name=implant_name)
+    plt.show()
+    
     
     # subdir = "/Volumes/large/BMI/VirtualReality/SpatialSequenceLearning/RUN_rYL006/rYL006_P1100/2025-01-27_13-39_rYL006_P1100_LinearTrackStop_73min"
     # data = read_raw_data(subdir, "ephys_output.raw.h5", convert2mV_float16=True, col_slice=slice(20_000, 30_000), to_df=True, subtract_dc_offset=True)

mea1k_modules/mea1k_raw_preproc.py
@@ -69,7 +69,7 @@ def _get_recording_config(path, fname):
 def _ADC2voltage(data, gain, subtract_dc_offset):
     L = Logger()
     resolution = _get_recording_resolution(gain)
-    dtype = np.int16 if gain in (1024,512,112) else np.int64
+    dtype = np.int16 if gain in (1024.0,512.0,112.0) else np.int32
     max_ampl_uV = int(resolution*ADC_RESOLUTION*1000)
     
     L.logger.debug(f"Converting ADC values to mV ({resolution:.4f}mV/ "
@@ -142,7 +142,13 @@ def read_stim_DAC(path, fname, col_slice=slice(None)):
 def read_raw_data(path, fname, convert2uV,
                   subtract_dc_offset=False, to_df=False, 
                   row_slice=slice(None), col_slice=slice(None)):
-    dtype = np.float16 if convert2uV else np.int16
+    
+    dtype = np.int16 # base case, no conversion of ADC values
+    if convert2uV:
+        dtype = np.float16 # uV of max ±28mV for gain=112, or smaller for higher gains
+        gain = _get_recording_gain(path, fname)
+        if gain in (1.0, 7.0):
+            dtype = np.float32 # uV get ±400 mV for gain=7.0, prevent overflow
     raw_data = _read_mea1k_file(path, fname, row_slice=row_slice, 
                                 col_slice=col_slice, dtype=dtype)
     
@@ -253,7 +259,7 @@ def get_recording_implant_mapping(path, mea1k_rec_fname, animal_name=None,
     L.logger.debug(f"Recording config:\n{rec_config}")
     
     # infer what implant was used, either from animal or through name itself
-    implant_mapping = _get_raw_implant_mapping(animal_name=animal_name,
+    implant_mapping = get_raw_implant_mapping(animal_name=animal_name,
                                                implant_name=implant_name)
     # get the general mapping from NAS, then reindex this table to fit the recording
     implant_mapping = implant_mapping.set_index('mea1k_el').reindex(rec_mea1k_els)
@@ -286,7 +292,7 @@ def get_recording_implant_mapping(path, mea1k_rec_fname, animal_name=None,
     Logger().logger.debug(f"Implant mapping after:\n{implant_mapping}")
     return implant_mapping
     
-def _get_raw_implant_mapping(implant_name=None, animal_name=None):
+def get_raw_implant_mapping(implant_name=None, animal_name=None):
     nas_dir = device_paths()[0]
     if animal_name is not None:
         implant_name = animal_name2implant_device(animal_name)
@@ -482,7 +488,7 @@ def replace_neuroscope_xml(session_dir, animal_name):
 def write_probe_file(subdir, fname, pad_size=11, shanks=[1.,2.]):
     data = read_raw_data(subdir, fname, convert2vol=True,  convert2uVInt=True,
                          col_slice=slice(0, 1000), subtract_dc_offset=True)
-    implant_mapping = _get_raw_implant_mapping(C.NAS_DIR, C.DEVICE_NAME)
+    implant_mapping = get_raw_implant_mapping(C.NAS_DIR, C.DEVICE_NAME)
     data, implant_mapping = assign_mapping_to_data(data, implant_mapping)
     
     # subset shanks

mea1k_modules/mea1k_visualizations.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import ephys_constants as C
 
 import colorsys
+import matplotlib.colorbar as mcolorbar
 
 def adjust_saturation(rgb_color, offset):
     # Convert RGB (0-1 scale) to HSV
@@ -20,7 +21,7 @@ def _n_sequentially_different_colors(n, cmap='tab20'):
     colors = [list(col) for col in cmap(np.linspace(0, 1, n))]
     return colors
 
-def vis_shank_traces(data, implant_mapping, scaler=80, stim_mea1k_el=None):
+def vis_shank_traces(data, implant_mapping, scaler=80, stim_mea1k_el=None, uVrange=6438):
     implant_mapping.reset_index(drop=True, inplace=True)
     # print(data)
     # print(implant_mapping)
@@ -75,11 +76,11 @@ def vis_shank_traces(data, implant_mapping, scaler=80, stim_mea1k_el=None):
         traces_iloc = np.where(implant_mapping.pad_id == pad_id)[0]
         stim_pad = np.isin(stim_mea1k_el, implant_mapping.iloc[traces_iloc].mea1k_el)
         
-        min_potential = depth -scaler*6438/2
-        max_potential = depth +scaler*6438/2
-        right_ax.text(data.shape[1], max_potential, f"0 mV", va='center', ha='left', fontsize=7)
-        right_ax.text(data.shape[1], min_potential, f"6.4 mV", va='center', ha='left', fontsize=7)
-        bg_rects.append(plt.Rectangle((0, min_potential), data.shape[1], scaler*6438,
+        min_potential = depth -scaler*uVrange/2
+        max_potential = depth +scaler*uVrange/2
+        right_ax.text(data.shape[1], max_potential, f"{-uVrange/1000/2:.1f} mV", va='center', ha='left', fontsize=7)
+        right_ax.text(data.shape[1], min_potential, f"{uVrange/1000/2:.1f} mV", va='center', ha='left', fontsize=7)
+        bg_rects.append(plt.Rectangle((0, min_potential), data.shape[1], scaler*uVrange,
                                       facecolor='black', alpha=.03 if not stim_pad else .15))
         
         for iloc in traces_iloc:
@@ -185,18 +186,17 @@ def draw_mea1k(bg='black', el_color='#111111'):
     if el_color == 'hsv':
         cmap = plt.get_cmap('hsv', 26400)
         colors = [list(col) for col in cmap(np.linspace(0, 1, 26400))]
+    elif isinstance(el_color, list):
+        colors = el_color
     else:
         colors = [el_color]*26400
-    
+
     i = 0
     recs = []
     # mea1k_yx = []
-    C.MEA1K_EL_2D_TABLE_PIXEL
     for y in np.arange(0+17.5/4, 2100, 17.5):
         for x in np.arange(0+17.5/4, 3850, 17.5):
-            recs.append(plt.Rectangle((x, y), 9, 
-                                      C.MEA1K_EL_HEIGHT_MICROMETER, 
-                                      facecolor=colors[i], 
+            recs.append(plt.Rectangle((x, y), 9, 9, facecolor=colors[i], 
                                       edgecolor='none', alpha=.7))
             # mea1k_yx.append((x+4.5,y+4.5))
             i += 1
@@ -215,7 +215,49 @@ def draw_mea1k(bg='black', el_color='#111111'):
     # plt.savefig(f"./el_pads_v3.png", dpi=300, transparent=True if bg=='transparent' else False, 
     #             bbox_inches='tight', pad_inches=0, )
     return (fig, ax), recs
- 
+
+def draw_mea1K_colorbar(cmap, norm, lbl, orientation='vertical'):
+    if orientation == 'vertical':
+        cbar_fig, cbar_ax = plt.subplots(figsize=(1.4, 2100/300))
+        cbar_fig.subplots_adjust(top=1, bottom=0, right=1, left=0)
+        cbar_fig.subplots_adjust(right=0.25, top=0.98, bottom=0.02)
+    elif orientation == 'horizontal':
+        cbar_fig, cbar_ax = plt.subplots(figsize=(2100/300, 1.4))
+        # cbar_fig.subplots_adjust(top=1, bottom=0, right=1, left=0)
+        cbar_fig.subplots_adjust(top=.95, bottom=.5)
+    else:
+        raise ValueError("Orientation must be either 'vertical' or 'horizontal'")
+    
+    cbar = mcolorbar.ColorbarBase(cbar_ax, cmap=cmap, norm=norm, orientation=orientation)
+    cbar.set_label(lbl)
+    return cbar_fig, cbar
+
+def draw_interconnect_pads(mapping, edgecolor='pad_id', pad_alpha=.6, 
+                           pad_scalar=1.5, draw_on_ax=None):
+    pad_circles, texts = [], []
+    for pad_id in sorted(mapping.pad_id.dropna().unique()):
+        pad_entry = mapping[mapping['pad_id'] == pad_id].iloc[0]
+        
+        if edgecolor == 'pad_id':
+            col = pad_entry[['r', 'g', 'b']].values/255
+        elif edgecolor=="metal":
+            col = 'purple' if pad_entry.metal == 2 else 'green'
+        elif isinstance(edgecolor, dict):
+            col = edgecolor.get(int(pad_id), [.3,.3,.3])
+        else:
+            col = edgecolor
+        
+        pad_circles.append(plt.Circle((pad_entry.x_aligned, pad_entry.y_aligned), 
+                                       pad_entry.pad_diameter_um/2 *pad_scalar, 
+                                       color=col,
+                                       fill=False, linewidth=2,
+                                       alpha=pad_alpha))
+        plt.text(pad_entry.x_aligned, pad_entry.y_aligned, f"{int(pad_id)}",
+                                fontsize=3, ha='center', va='center', color='white')
+    if draw_on_ax is not None:
+        [draw_on_ax.add_patch(pc) for pc in pad_circles]
+    return pad_circles
+
 def viz_mea1k_config(pad_alignment, stim_mea1k_el=None):
      # darw mewa1k
     